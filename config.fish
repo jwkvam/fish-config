974b06cee492aa4cc372ad41c9725397cdb08c98
Jacques Kvam <jacques@verdigris.co>
2016-08-25 14:17:04 -0700

    update

--- config.fish
+++ config.fish
@@ -1,32 +1,32 @@
-set PATH /Users/jacques/miniconda/bin /Developer/NVIDIA/CUDA-7.0/bin /Users/jacques/.local/bin /usr/local/bin $PATH
-set DYLD_LIBRARY_PATH /Developer/NVIDIA/CUDA-7.0/lib $DYLD_LIBRARY_PATH
+# set PATH /Users/jacques/miniconda/bin /Developer/NVIDIA/CUDA-7.0/bin /Users/jacques/.local/bin /usr/local/bin $PATH
+set PATH /Users/jacques/miniconda/bin /Users/jacques/.local/bin /usr/local/bin $PATH
+#set DYLD_LIBRARY_PATH /Developer/NVIDIA/CUDA-7.0/lib $DYLD_LIBRARY_PATH
 set DYLD_FALLBACK_LIBRARY_PATH $DYLD_FALLBACK_LIBRARY_PATH /Users/jacques/miniconda/lib
-set EDITOR vi
+set EDITOR nvim
 set NVIM_TUI_ENABLE_TRUE_COLOR 1
 
-set -x PYTHONPATH ~/dev/apollo
-
 set -x DJANGO_SETTINGS_MODULE apollo.settings.data
+set -x PYTHONPATH ~/dev/apollo
+set -x PYTHONPATH ~/dev/rover:$PYTHONPATH
 
-alias sshb="ssh jacques@10.10.10.127"
-# alias git="hub"
-
+alias sshb="ssh jacques@harmonic.local"
 alias ff="find . -name"
 alias vi="nvim"
 
 alias i=ipython
-alias pyld="pylint --load-plugins pylint_django"
 
-function fish_user_key_bindings
-    fish_vi_key_bindings
+fish_vi_key_bindings
 
-    # bind -M insert \ck backward-kill-line
-    bind -M insert \ck kill-whole-line
-    bind -M insert \cl 'clear; commandline -f repaint'
-    bind -M insert \cf accept-autosuggestion
-    bind -M insert \ca beginning-of-line
-    bind -M insert \ce end-of-line
-end
+# function fish_user_key_bindings
+#     fish_vi_key_bindings
+#
+#     # bind -M insert \ck backward-kill-line
+#     bind -M insert \ck kill-whole-line
+#     bind -M insert \cl 'clear; commandline -f repaint'
+#     bind -M insert \cf accept-autosuggestion
+#     bind -M insert \ca beginning-of-line
+#     bind -M insert \ce end-of-line
+# end
 
 function -e fish_preexec _run_fasd
     # fasd --proc (fish_split (fasd --sanitize $argv)) > /dev/null 2>&1
@@ -46,10 +46,8 @@ function e
     fasd -fe vim "$argv"
 end
 
-. /Users/jacques/.config/fish/jenv.fish
-# . /Users/jacques/.config/fish/j.fish
-. /Users/jacques/.config/fish/conda.fish
-. /Users/jacques/.config/fish/functions/fzf.fish
-. /Users/jacques/.config/fish/functions/fzf_wrap.fish
+function vf
+    fzf > /tmp/fzf.result; and nvim (cat /tmp/fzf.result)
+end
 
-. /Users/jacques/.config/fish/informative_git.fish
+source (conda info --root)/etc/fish/conf.d/conda.fish
